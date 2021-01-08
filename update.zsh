a5351071a2a737ed89a7645501db5a6628591eec
Queensferry <queensferry.me@gmail.com>
2021-01-08 09:57:24 +0800

    update update.zsh

--- update.zsh
+++ update.zsh
@@ -2,6 +2,20 @@
 
 source $HOME/.zshrc
 
+# Configure proxy server
+PROXY="http://127.0.0.1:7890"
+export http_proxy=$PROXY
+export https_proxy=$PROXY
+git config --global http.proxy $PROXY
+git config --global https.proxy $PROXY
+
+# Upgrade Brew
+if [[ `command -v brew` != "" ]]; then
+    echo '========== Upgrading Brew =========='
+    brew update
+    brew upgrade
+fi
+
 # Upgrade N
 if [ -d $HOME/.n ]; then
     echo '========== Upgrading N =========='
@@ -24,10 +38,22 @@ if [ -d $HOME/.pyenv ]; then
     git pull
 fi
 
+# Upgrade Rust
+if [ -d $HOME/.cargo ]; then
+    echo '========== Upgrading Rust =========='
+    rustup self update
+    rustup update
+fi
+
 # Upgrade Zinit
 if [ -d $HOME/.zinit ]; then
     echo '========== Upgrading Zinit =========='
     zinit update
 fi
 
+# End
+unset http_proxy
+unset https_proxy
+git config --global --unset http.proxy
+git config --global --unset https.proxy
 echo `date` 'Update successful!'
