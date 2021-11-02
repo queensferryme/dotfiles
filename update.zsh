3074d438883cc46063b28c450a036602b96a6f3d
Queensferry <queensferry.me@gmail.com>
2021-11-02 21:11:35 +0800

    migrate from `zinit` to `sheldon`

--- update.zsh
+++ update.zsh
@@ -2,18 +2,11 @@
 
 source $HOME/.zshrc
 
-# Upgrade MacPorts
+# Upgrade Homebrew
 if [ $(command -v brew) != "" ]; then
     echo '========== Upgrading Brew =========='
     brew upgrade
     brew cleanup -s
-fi
-
-# Upgrade N
-if [ -d $HOME/.n ]; then
-    echo '========== Upgrading N =========='
-    n lts
-    n prune
 fi
 
 # Upgrade NeoVim
@@ -22,27 +15,17 @@ if [ $(command -v nvim) != "" ]; then
     nvim +PlugUpgrade +PlugUpdate +qall
 fi
 
-# Upgrade Poetry
-if [ -d $HOME/.poetry ]; then
-    echo '========== Upgrading Poetry =========='
-    poetry self update
-    poetry completions zsh > $HOME/.zinit/completions/_poetry
-fi
-
 # Upgrade Rust
-if [ -d $HOME/.cargo ]; then
+if [ $(command -v rustup) != "" ]; then
     echo '========== Upgrading Rust =========='
     rustup self update
     rustup update
-    rustup completions zsh cargo > $HOME/.zinit/completions/_cargo
-    rustup completions zsh rustup > $HOME/.zinit/completions/_rustup
 fi
 
-# Upgrade Zinit
-if [ -d $HOME/.zinit ]; then
-    echo '========== Upgrading Zinit =========='
-    zinit self-update
-    zinit update --all
+# Upgrade Sheldon
+if [ $(command -v sheldon) != "" ]; then
+    echo '========== Upgrading Sheldon =========='
+    sheldon lock --update
 fi
 
 # End
