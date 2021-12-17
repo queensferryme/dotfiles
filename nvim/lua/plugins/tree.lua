359c4201447eef6dfd2bde418358eedf24a7573a
Queensferry <queensferry.me@gmail.com>
2021-12-17 15:38:18 +0800

    improve nvim-tree view width config

--- nvim/lua/plugins/tree.lua
+++ nvim/lua/plugins/tree.lua
@@ -1,30 +1,7 @@
-vim.api.nvim_set_keymap('n', '<C-n>', ':lua NvimTreeToggle()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', '<C-n>', '<C-O>:lua NvimTreeToggle()<CR>', { noremap = true, silent = true })
-function NvimTreeToggle()
-    local bufferline = require('bufferline.state')
-    local tree = require('nvim-tree')
-    if require('nvim-tree.view').win_open() then
-        bufferline.set_offset(0)
-        tree.close()
-    else
-        bufferline.set_offset(NvimTreeWidth(), 'NvimTree')
-        tree.open()
-    end
-end
+vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<C-n>', '<C-O>:NvimTreeToggle<CR>', { noremap = true, silent = true })
 
 vim.cmd [[autocmd CursorHold,CursorHoldI * :NvimTreeRefresh]]
--- TODO: https://github.com/kyazdani42/nvim-tree.lua/issues/770
-vim.cmd [[autocmd VimResized * lua require('nvim-tree.view').View.width=NvimTreeWidth()]]
-function NvimTreeWidth()
-    local winwidth = vim.fn.winwidth(0)
-    if winwidth <= 100 then
-        return 30
-    elseif winwidth <= 200 then
-        return 40
-    else
-        return 50
-    end
-end
 
 vim.g.nvim_tree_group_empty = 1
 vim.g.nvim_tree_highlight_opened_files = 1
@@ -40,7 +17,16 @@ require('nvim-tree').setup {
     diagnostics = { enable = true },
     filters = { dotfiles = true },
     view = {
-        width = NvimTreeWidth(),
+        width = function ()
+            local winwidth = vim.fn.winwidth(0)
+            if winwidth <= 100 then
+                return 30
+            elseif winwidth <= 200 then
+                return 45
+            else
+                return 60
+            end
+        end,
         hide_root_folder = true,
     },
 }
